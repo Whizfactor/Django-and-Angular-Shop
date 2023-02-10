0fddd404b2a8eaf85ba1aab67082cba643f0eba9
Whizfactor <65239378+Whizfactor@users.noreply.github.com>
2023-02-10 20:39:29 +0100

    1 file modified

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,9 +5,15 @@ import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 
 import {PagesModule} from './pages/pages.module';
+import { AppRoutingModule } from './app-routing.module';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule, PagesModule ],
+  imports: [ 
+    BrowserModule, 
+    FormsModule, 
+    PagesModule, 
+    AppRoutingModule
+  ],
   declarations: [ AppComponent ],
   bootstrap:    [ AppComponent ]
 })
