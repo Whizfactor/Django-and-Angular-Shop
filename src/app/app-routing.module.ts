14413b94d9c12add40863b4dee4d02a2ec0cacde
Whizfactor <65239378+Whizfactor@users.noreply.github.com>
2023-02-10 23:05:14 +0100

    5 files modified, 2 added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { AboutComponent } from './pages/about/about.component';
+import { PageNotFoundComponent } from './pages/page-not-found/page-not-found.component';
 
 const routes: Routes = [
-  
+  { path: 'home', component: HomeComponent },
+  { path: 'about', component: AboutComponent },
+  { path: 'page-not-found', component: PageNotFoundComponent },
+  { path: '', pathMatch: 'full', redirectTo: 'home' }, // redirect to HomeComponent
+  { path: '**', component: PageNotFoundComponent },  // Wildcard route for a 404 page
+
 ]
 
 @NgModule({
